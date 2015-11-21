e05e19212b9e7b4ba70af819951b7d31abdefa59
jessejtf <critterguy@gmail.com>
2015-11-21 18:15:53 +0000

    Fixed some rndom stuff

--- config/environment.rb
+++ config/environment.rb
@@ -8,8 +8,8 @@ ActionMailer::Base.smtp_settings = {
     :address => 'smtp.sendgrid.net',
     :port => '587',
     :authentication => :plain,
-    :user_name => ENV['app44145951@heroku.com'],
-    :password => ENV['ms2ukpbg0181'],
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
     :domain => 'heroku.com',
     :enable_startstls_auto => true
 }
