6219fb296c0bc35e56e9c71ad60e6b24fd9a7f10
jessejtf <critterguy@gmail.com>
2015-11-21 14:05:46 +0000

    fixed some typos,

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,10 +8,10 @@ class ContactsController < ApplicationController
         
         if @contact.save
             name = params[:contact][:name]
-            email params[:contact][:email]
+            email = params[:contact][:email]
             body = params[:contact][:comments]
             
-            ContractMailer.contact_email(name, email, body).deliver
+            ContactMailer.contact_email(name, email, body).deliver
             flash[:success] =  "Message Sent"
             redirect_to new_contact_path
         else
