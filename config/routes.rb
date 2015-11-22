112ea44fa4729fd5b5b54dba2fb1eb4d34f0bb6c
jessejtf <critterguy@gmail.com>
2015-11-22 05:17:58 +0000

    added plans kinda

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  root 'pages#home'
   devise_for :users
  resources :contacts
   get '/about' => 'pages#about'
-  root 'pages#home'
 
 
   # The priority is based upon order of creation: first created -> highest priority.
